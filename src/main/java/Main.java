2a093243e3faf87ba5055e10c894d396d4cf8b6f
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-04-08 17:53:29 +0200

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+        Heartbeat heartbeat = new Heartbeat();
+
+        heartbeat.sendHeartbeat();
+
+    }
+}
+
+
