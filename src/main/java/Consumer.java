d90c924f5a1df3f8bc7065524a4e040d3e26e74e
JAATAR <133091960+JAATAR@users.noreply.github.com>
2024-04-30 22:12:37 +0200

    feat: xml files convert to Java objects

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,5 +1,9 @@
 import com.rabbitmq.client.*;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class Consumer {
@@ -46,11 +50,51 @@ public class Consumer {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8"); //convert byte array in string
                 System.out.println(" [x] Received '" + message + "'");
+
+                try {
+                    // Unmarshall de XML naar de  objecten
+                    Participant participant = unmarshalParticipant(message);
+                    Event event = unmarshalEvent(message);
+                    Session session = unmarshalSession(message);
+                    Business business = unmarshalBusiness(message);
+
+                } catch (JAXBException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
         // start consuming messages from the queue
         channel.basicConsume(queueName, true, consumer);
+    }
+    // Unmarshall Participant-object van XML-string
+    private Participant unmarshalParticipant(String xml) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Participant.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+        return (Participant) jaxbUnmarshaller.unmarshal(inputStream);
+    }
+    // Unmarshall Event-object van XML-string
+    private Event unmarshalEvent(String xml) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Event.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+        return (Event) jaxbUnmarshaller.unmarshal(inputStream);
+    }
+    // Unmarshall Session-object van XML-string
+    private Session unmarshalSession(String xml) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Session.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+        return (Session) jaxbUnmarshaller.unmarshal(inputStream);
+    }
+
+    // Unmarshall Business-object van XML-string
+    private Business unmarshalBusiness(String xml) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Business.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+        return (Business) jaxbUnmarshaller.unmarshal(inputStream);
     }
 }
 
