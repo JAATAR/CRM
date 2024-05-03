5b45dbaf0fcaeca3b7661e6d51b5f8efa934ba3e
JAATAR <133091960+JAATAR@users.noreply.github.com>
2024-05-03 18:50:04 +0200

    Update Main class with JAXB unmarshalling functionality and refactor printParticipantDetails method in Session class
    multiple changes:
    - Add JAXB-based XML unmarshalling functionality in Main class
    - Fix NullPointerException in JAXB unmarshalling process
    - Refactor printParticipantDetails method in Session class to improve readability

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -52,50 +52,97 @@ public class Consumer {
                 System.out.println(" [x] Received '" + message + "'");
 
                 try {
+
                     // Unmarshall de XML naar de  objecten
                     Participant participant = unmarshalParticipant(message);
                     Event event = unmarshalEvent(message);
                     Session session = unmarshalSession(message);
                     Business business = unmarshalBusiness(message);
 
+                    if(unmarshalParticipant(message) instanceof Participant){
+                        Participant participant1 = (Participant) unmarshalParticipant(message);
+                    }
+                    else if(unmarshalEvent(message) instanceof Event)
+                    {
+                        Event event1 = (Event) unmarshalEvent(message);
+                    }
+                    else if(unmarshalBusiness(message) instanceof Business){
+
+                        Business business1 = (Business) unmarshalBusiness(message);
+
+                    } else if (unmarshalSession(message) instanceof Session){
+
+                        Session session1 = (Session) unmarshalSession(message);
+                    }
+
                 } catch (JAXBException e) {
                     e.printStackTrace();
                 }
             }
+
         };
 
         // start consuming messages from the queue
         channel.basicConsume(queueName, true, consumer);
     }
+    // Unmarshal XML to corresponding objects based on its type
+    public Object unmarshalBasedOnType(String xml) throws JAXBException {
+        JAXBContext jaxbContext = null;
+        Unmarshaller jaxbUnmarshaller = null;
+
+        if (xml.contains("<participant")) {
+            jaxbContext = JAXBContext.newInstance(Participant.class);
+        } else if (xml.contains("<event")) {
+            jaxbContext = JAXBContext.newInstance(Event.class);
+        } else if (xml.contains("<session")) {
+            jaxbContext = JAXBContext.newInstance(Session.class);
+        } else if (xml.contains("<business")) {
+            jaxbContext = JAXBContext.newInstance(Business.class);
+        }
+
+        if (jaxbContext != null) {
+            jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+            return jaxbUnmarshaller.unmarshal(inputStream);
+        }
+
+        return null;
+    }
     // Unmarshall Participant-object van XML-string
-    private Participant unmarshalParticipant(String xml) throws JAXBException {
+    public Participant unmarshalParticipant(String xml) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Participant.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
         return (Participant) jaxbUnmarshaller.unmarshal(inputStream);
+
     }
     // Unmarshall Event-object van XML-string
-    private Event unmarshalEvent(String xml) throws JAXBException {
+    public Event unmarshalEvent(String xml) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Event.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
         return (Event) jaxbUnmarshaller.unmarshal(inputStream);
+
     }
     // Unmarshall Session-object van XML-string
-    private Session unmarshalSession(String xml) throws JAXBException {
+    public Session unmarshalSession(String xml) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Session.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
         return (Session) jaxbUnmarshaller.unmarshal(inputStream);
+
     }
 
     // Unmarshall Business-object van XML-string
-    private Business unmarshalBusiness(String xml) throws JAXBException {
+    public Business unmarshalBusiness(String xml) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(Business.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
         return (Business) jaxbUnmarshaller.unmarshal(inputStream);
+
+
     }
+
 }
 
 
--- src/main/java/Session.java
+++ src/main/java/Session.java
@@ -1,17 +1,21 @@
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlElementWrapper;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
 
 import java.util.List;
+
 @XmlRootElement
 public class Session {
     @XmlElementWrapper(name = "speakers")
     @XmlElement(name = "participant")
     private List<Participant> speakers;
+
     @XmlElementWrapper(name = "participants")
     @XmlElement(name = "participant")
     private List<Participant> participants;
 
+    @XmlTransient
     public List<Participant> getSpeakers() {
         return speakers;
     }
@@ -20,6 +24,8 @@ public class Session {
         this.speakers = speakers;
     }
 
+
+    @XmlTransient
     public List<Participant> getParticipants() {
         return participants;
     }
@@ -27,4 +33,15 @@ public class Session {
     public void setParticipants(List<Participant> participants) {
         this.participants = participants;
     }
+
+    public Session printParticipantDetails() {
+        System.out.println("Participants:");
+        for (Participant participant : participants) {
+            System.out.println("Name: " + participant.getFirstname() + " " + participant.getLastname());
+            System.out.println("Email: " + participant.getEmail());
+
+        }
+        return printParticipantDetails();
+    }
+
 }
