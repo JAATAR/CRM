09b15c3549ef3c9947498ab91d9cba4764fec2e1
Soufiane Hamoumi <soufiane.hamoumi@student.ehb.be>
2024-05-04 17:33:58 +0200

    consumer connection
    laatse weiziging

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,10 +1,18 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
+
 import com.force.api.ApiConfig;
 import com.force.api.ForceApi;
 import com.rabbitmq.client.*;
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import java.io.IOException;
+
 
 public class Consumer {
 
@@ -57,37 +65,49 @@ public class Consumer {
         channel.basicConsume(queueName, true, consumer);
         System.out.println("ready to consumer");
     }
-    public void saveToSalesforce() {
-        String SALESFORCE_USERNAME = "ehbevent@student.ehb.be";
-        String SALESFORCE_PASSWORD = "5431Event";
-        String SALESFORCE_SECURITY_TOKEN = "NZTge2bWfzzVvxf8rZkPKOhA";
-        String LOGIN_URL = "https://erasmushogeschool7-dev-ed.develop.my.salesforce.com";
-        String CONSUMER_KEY = "3MVG9PwZx9R6_UrfopP9UuSYm9.9btZdAiMG6rKyTdaV8nUXzfEiZJ9oT9XyY4lKvsxSv0W9L28QibW7MWtmD";
-        String CONSUMER_SECRET = "8F01848AA8E6016D0D1EEA3DC0BA2C0B270C0EF3106DFFAEE09CC384F058B10C";
 
-        // Combineer wachtwoord en beveiligingstoken
-        String loginPassword = SALESFORCE_PASSWORD + SALESFORCE_SECURITY_TOKEN;
 
-        // Configureer de API-configuratie
-        ApiConfig config = new ApiConfig()
-                .setClientId(CONSUMER_KEY)
-                .setClientSecret(CONSUMER_SECRET)
-                .setUsername(SALESFORCE_USERNAME)
-                .setPassword(loginPassword)
-                .setLoginEndpoint(LOGIN_URL);
 
-        ForceApi api = new ForceApi(config);
+        public void connectToSalesforceAndSendData() {
+            String SALESFORCE_USERNAME = "hamza.amghar@student.ehb.be";
+            String SALESFORCE_PASSWORD = "Event5431";
+            String SALESFORCE_SECURITY_TOKEN = "QdhyxDHRs9hyqkvhKwQfz4aLl";
+            String LOGIN_URL = "https://erasmushogeschool7-dev-ed.develop.my.salesforce.com";
+            String CONSUMER_KEY = "3MVG9PwZx9R6_UrfopP9UuSYm9.9btZdAiMG6rKyTdaV8nUXzfEiZJ9oT9XyY4lKvsxSv0W9L28QibW7MWtmD";
+            String CONSUMER_SECRET = "8F01848AA8E6016D0D1EEA3DC0BA2C0B270C0EF3106DFFAEE09CC384F058B10C";
 
-        // Maak de gegevens voor de aan te maken Deelnemer
-        Map<String, Object> deelnemerFields = new HashMap<>();
-        deelnemerFields.put("Name", "Mike Tyson");
-        deelnemerFields.put("Leeftijd__c", 25);
-        deelnemerFields.put("Nummertelefoon__c", "0485009987");
-        deelnemerFields.put("Email__c", "miketyson@gmail.com");
+            // Combineer wachtwoord en beveiligingstoken
+            String loginPassword = SALESFORCE_PASSWORD + SALESFORCE_SECURITY_TOKEN;
 
-        // Maak de Deelnemer aan in Salesforce
-        api.createSObject("Deelnemer__c", deelnemerFields);
-    }
+            // Configureer de API-configuratie
+            ApiConfig config = new ApiConfig()
+                    .setClientId(CONSUMER_KEY)
+                    .setClientSecret(CONSUMER_SECRET)
+                    .setUsername(SALESFORCE_USERNAME)
+                    .setPassword(SALESFORCE_PASSWORD)
+                    .setLoginEndpoint(LOGIN_URL);
+
+            ForceApi api = new ForceApi(config);
+            System.out.printf("ik werk ");
+            // Maak de gegevens voor de aan te maken Deelnemer
+            Map<String, Object> deelnemerFields = new HashMap<>();
+            System.out.println("test een ");
+            deelnemerFields.put("Name", "Mike Tyson");
+            System.out.println("test twee");
+            deelnemerFields.put("Leeftijd__c", 25);
+            System.out.println("test drie ");
+            deelnemerFields.put("Nummertelefoon__c", "0485009987");
+            System.out.println("test vier ");
+            deelnemerFields.put("Email__c", "miketyson@gmail.com");
+            System.out.println("test vijf ");
+            deelnemerFields.put("Bedrijf__c", "erasmus");
+            System.out.println("test last ");
+
+            // Maak de Deelnemer aan in Salesforce
+            api.createSObject("Deelnemer__c", deelnemerFields);
+            System.out.println("object createt");
+        }
+
 
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,24 +7,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        // we create a ScheduledExecutorService to schedule the heartbeats to be sent within an interval
-      ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1); // use of one thread to execute the scheduled heartbeats
 
-        // we schedule the task to send heartbeats every second
-        heartbeatScheduler.scheduleAtFixedRate(() -> {
-            try {
-                Heartbeat heartbeat = new Heartbeat();
-                heartbeat.sendHeartbeat();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, 0, 5, TimeUnit.SECONDS);
+//        // we create a ScheduledExecutorService to schedule the heartbeats to be sent within an interval
+//      ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1); // use of one thread to execute the scheduled heartbeats
+//
+//        // we schedule the task to send heartbeats every second
+//        heartbeatScheduler.scheduleAtFixedRate(() -> {
+//            try {
+//                Heartbeat heartbeat = new Heartbeat();
+//                heartbeat.sendHeartbeat();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }, 0, 5, TimeUnit.SECONDS);
 
 
         Consumer consumer = new Consumer();
-        consumer.startConsuming();
+//        consumer.startConsuming();
+//        // Connecteer met Salesforce en stuur gegevens
+        consumer.connectToSalesforceAndSendData();
 
-       consumer.saveToSalesforce();
+
+
 
     }
 }
