7bf490ec6e09a6bf920a73003734f0e644399116
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-05-09 16:48:12 +0200

    complete refactor of project

---
+++ src/main/java/crm/Main.java
@@ -0,0 +1,29 @@
+package crm;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        // we create a ScheduledExecutorService to schedule the heartbeats to be sent within an interval
+        ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1); // use of one thread to execute the scheduled heartbeats
+
+        // we schedule the task to send heartbeats every second
+        heartbeatScheduler.scheduleAtFixedRate(() -> {
+            try {
+                Heartbeat heartbeat = new Heartbeat();
+                heartbeat.sendHeartbeat();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+
+        Consumer consumer = new Consumer();
+        consumer.startConsuming();
+
+    }
+}
