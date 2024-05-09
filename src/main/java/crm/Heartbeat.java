8b43df5ad0543db68c3d1036d3653ad165f467d8
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-05-09 14:37:39 +0200

    test

--- src/main/java/crm/Heartbeat.java
+++ src/main/java/crm/Heartbeat.java
@@ -97,13 +97,13 @@ public class Heartbeat {
                 "</heartbeat>";
 
 
-        if (!validateXML(realXml,xsd)){
+       // if (!validateXML(realXml,xsd)){
 
-            System.out.println("XML validation failed. crm.Heartbeat not sent");
-            return null; // if validation fails the method stops and heartbeat is not sent
-        }
+        //    System.out.println("XML validation failed. crm.Heartbeat not sent");
+      //      return null; // if validation fails the method stops and heartbeat is not sent
+     //   }
 
-        System.out.println("validation succesful");
+      //  System.out.println("validation succesful");
 
         if (this.getError() == 1){
 
