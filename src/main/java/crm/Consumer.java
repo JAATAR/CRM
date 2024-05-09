064f71960ac216adbcdea44ca3551f6b030c357d
Soufiane Hamoumi <soufiane.hamoumi@student.ehb.be>
2024-05-09 20:46:55 +0200

    fix api

--- src/main/java/crm/Consumer.java
+++ src/main/java/crm/Consumer.java
@@ -1,5 +1,7 @@
 package crm;
 
+import com.force.api.ApiConfig;
+import com.force.api.ForceApi;
 import com.rabbitmq.client.*;
 import crm.Business;
 import jakarta.xml.bind.JAXBContext;
@@ -16,6 +18,9 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Consumer {
     private final String CONSUMING_QUEUE = System.getenv("CONSUMING_QUEUE");
@@ -169,6 +174,72 @@ public class Consumer {
 
         return true;
     }
+    public static ForceApi connectToSalesforce() {
+        String SALESFORCE_USERNAME = "ehberasmus@gmail.com";
+        String SALESFORCE_PASSWORD = "Event5431";
+        String SALESFORCE_SECURITY_TOKEN = "S4lOdXADEdHNLYorrabi2mLg";
+        String LOGIN_URL = "https://ehb-dev-ed.develop.my.salesforce.com";
+        String CONSUMER_KEY = "3MVG9PwZx9R6_Urc1GPWYVjQmwHmXKY1pQ8t_W_Ql4VXOFeo_9tKJW3O8nLf0JJoMjrOuii6wZ8XdpCcJfOOA";
+        String CONSUMER_SECRET = "BA7D5B9E3434948E1751C3C5B51BC366B8FD9165E3DCBD95A62AEF4D06B5C4C9";
+
+        // Combineer wachtwoord en beveiligingstoken
+        String loginPassword = SALESFORCE_PASSWORD + SALESFORCE_SECURITY_TOKEN;
+
+        // Configureer de API-configuratie
+        ApiConfig config = new ApiConfig()
+                .setClientId(CONSUMER_KEY)
+                .setClientSecret(CONSUMER_SECRET)
+                .setUsername(SALESFORCE_USERNAME)
+                .setPassword(loginPassword)
+                .setLoginEndpoint(LOGIN_URL);
+
+        ForceApi api = new ForceApi(config);
+//            createDeelnemer(api);
+//            createBusiness(api);
+//            createConsumption(api);
+        return api;
+
+    }
+
+    public static void createDeelnemer() {
+        ForceApi api = connectToSalesforce();
+
+        Map<String, Object> deelnemerFields = new HashMap<>();
+        deelnemerFields.put("Name", "marcelo");
+        deelnemerFields.put("Leeftijd__c", 32);
+        deelnemerFields.put("Phone__c", "0485009999");
+        deelnemerFields.put("Email__c", "marcelo@gmail.com");
+        deelnemerFields.put("Bedrijf__c", "real madrid");
+
+        // Maak de Deelnemer aan in Salesforce
+        api.createSObject("Deelnemer__c", deelnemerFields);
+    }
+
+    public static void createBusiness() {
+        ForceApi api = connectToSalesforce();
+        Map<String, Object> businessFields = new HashMap<>();
+        businessFields.put("Name", "business soso");
+        businessFields.put("VAT__c", "123456999");
+        businessFields.put("Email__c", "soso@bedrijf.com");
+        businessFields.put("Access_Code__c", 2455);
+        businessFields.put("Address__c", "sosostraat 128");
+
+        // Maak het Business object aan in Salesforce
+        api.createSObject("Business__c", businessFields);
+    }
+
+    public static void createConsumption() {
+        ForceApi api = connectToSalesforce();
+        Map<String, Object> consumptionFields = new HashMap<>();
+        consumptionFields.put("Timestamp__c", new Date());
+        consumptionFields.put("Name", "food");
+        consumptionFields.put("Products__c", "hotdog");
+        consumptionFields.put("Consumer__c", "het is lekker "); // Voorbeeld UUID
+
+        // Maak het Consumption object aan in Salesforce
+        api.createSObject("Consumption__c", consumptionFields);
+    }
+
 
 }
 
