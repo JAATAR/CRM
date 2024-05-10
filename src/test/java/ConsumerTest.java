ac7be4d67c2034a9df06e759ba763e2d7f9b181f
Hamza <hamza.amghar@student.ehb.be>
2024-05-10 22:20:17 +0200

    t

--- src/test/java/ConsumerTest.java
+++ src/test/java/ConsumerTest.java
@@ -4,84 +4,81 @@ import crm.Consumption;
 import crm.Participant;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ConsumerTest {
+class ConsumerTest {
 
     @Test
-    void unmarshalParticipant() throws Exception {
-        // Arrange
+    void unmarshalBasedOnType_shouldUnmarshalParticipant() throws JAXBException, IOException, jakarta.xml.bind.JAXBException {
+
+        String xml = "<participant><firstname>John</firstname><lastname>Doe</lastname></participant>";
         Consumer consumer = new Consumer();
-        String xml = "<participant><firstname>John</firstname><lastname>Doe</lastname><age>30</age></participant>";
 
-        // Act
-        Participant participant = consumer.unmarshalParticipant(xml);
 
-        // Assert
-        assertEquals("John", participant.getFirstname());
-        assertEquals("Doe", participant.getLastname());
-        assertEquals(30, participant.getAge());
+        Object result = consumer.unmarshalBasedOnType(xml);
+
+
+        assertInstanceOf(Participant.class, result);
     }
 
     @Test
-    void unmarshalBusiness() throws Exception {
-        // Arrange
+    void unmarshalBasedOnType_shouldUnmarshalBusiness() throws JAXBException, IOException, jakarta.xml.bind.JAXBException {
+
+        String xml = "<business><name>ABC Company</name><vat>123456789</vat></business>";
         Consumer consumer = new Consumer();
-        String xml = "<business><name>ABC Corp</name><vat>123456789</vat></business>";
 
-        // Act
-        Business business = consumer.unmarshalBusiness(xml);
 
-        // Assert
-        assertEquals("ABC Corp", business.getName());
-        assertEquals("123456789", business.getVat());
+        Object result = consumer.unmarshalBasedOnType(xml);
+
+
+        assertInstanceOf(Business.class, result);
     }
 
     @Test
-    void unmarshalConsumption() throws Exception {
-        // Arrange
+    void unmarshalBasedOnType_shouldUnmarshalConsumption() throws JAXBException, IOException, jakarta.xml.bind.JAXBException {
+
+        String xml = "<consumption><products>Product A, Product B</products></consumption>";
         Consumer consumer = new Consumer();
-        String xml = "<consumption><timestamp>2024-05-09T21:00:00</timestamp><name>food</name></consumption>";
 
-        // Act
-        Consumption consumption = consumer.unmarshalConsumption(xml);
 
-        // Assert
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        Date expectedDate = sdf.parse("2024-05-09T21:00:00");
+        Object result = consumer.unmarshalBasedOnType(xml);
 
-        assertEquals(expectedDate, consumption.getTimestamp());
-        assertEquals("food", consumption.getProducts());
+
+        assertInstanceOf(Consumption.class, result);
     }
 
     @Test
-    void validateXML_ValidXML() {
-        // Arrange
-        String xml = "<participant><firstname>John</firstname><lastname>Doe</lastname><age>30</age></participant>";
+    void validateXML_shouldReturnTrueForValidXML() {
+
+        String xml = "<participant><firstname>John</firstname><lastname>Doe</lastname></participant>";
         String xsdPath = "src/main/resources/include.template.xsd";
 
-        // Act
-        boolean isValid = Consumer.validateXML(xml, xsdPath);
 
-        // Assert
-        assertTrue(isValid);
+        boolean result = Consumer.validateXML(xml, xsdPath);
+
+
+        assertTrue(result);
     }
 
     @Test
-    void validateXML_InvalidXML() {
-        // Arrange
-        String xml = "<participant><firstname>John<firstname><lastname>Doe</lastname><age>30</age></participant>";
+    void validateXML_shouldReturnFalseForInvalidXML() {
+
+        String xml = "<participant><firstname>John</firstname><lastname>Doe</lastname>";
         String xsdPath = "src/main/resources/include.template.xsd";
 
-        // Act
-        boolean isValid = Consumer.validateXML(xml, xsdPath);
 
-        // Assert
-        assertFalse(isValid);
+        boolean result = Consumer.validateXML(xml, xsdPath);
+
+
+        assertFalse(result);
     }
+
+
+
+
+
 }
